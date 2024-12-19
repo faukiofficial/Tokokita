e69f97e7eee37d430bd15d630566d9ade3629842
faukiofficial <faukiofficial@gmail.com>
2024-12-20 01:34:18 +0700

    uninstall unused package

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,15 +4,12 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/srote.js";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 import {Toaster} from "react-hot-toast"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
-      <ToastContainer />
       <Toaster position="bottom-right" />
     </Provider>
   </BrowserRouter>
